6804131d3309e6dedffcbbf2d2bf4e4bdb410dd0
AswaniSVV <aswani@empacto.in>
2025-10-04 09:51:03 +0900

    Delete horizons-export-c65dc6bb-30ef-40a3-9e92-955710ebbe07 (4) directory

--- horizons-export-c65dc6bb-30ef-40a3-9e92-955710ebbe07 (4)/src/lib/customSupabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://fefrqucgeiztzmyzgioc.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZlZnJxdWNnZWl6dHpteXpnaW9jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg2NzY5MzAsImV4cCI6MjA3NDI1MjkzMH0.273Rg6hFgBSNOLE2T4C0siOaCeAiEHw8Htmg1X0HntQ';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
